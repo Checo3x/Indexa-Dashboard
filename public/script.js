ad2b26d87c3781a97937e47c68021e22b3e509bb
Sergio Martín Calero <101972076+Checo3x@users.noreply.github.com>
2025-04-23 19:14:59 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ if (typeof Chart === 'undefined') {
 }
 
 const colorPalette = [
-    '#FF6B6B', '#4ECDC4', '#45B7D1', '#96CEB4', '#FFEEAD',
+    '#FF6B6B', '#45B7D1', '#96CEB4', '#FFEEAD',
     '#D4A5A5', '#9B59B6', '#3498DB', '#E74C3C', '#2ECC71'
 ];
 
