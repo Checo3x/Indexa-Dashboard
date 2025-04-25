5107ffb096e6956be7fdbdc217104d9c9ae77c34
Sergio Martín Calero <101972076+Checo3x@users.noreply.github.com>
2025-04-25 23:02:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
-export default {
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    root: 'public', // Especificamos que la raíz del proyecto es la carpeta 'public'
     server: {
         port: 3000,
         open: true,
@@ -11,6 +15,8 @@ export default {
         }
     },
     build: {
-        outDir: 'dist'
-    }
-}
+        outDir: '../dist', // Ajustamos el directorio de salida para que sea relativo a la raíz del proyecto
+        emptyOutDir: true // Asegura que el directorio de salida se limpie antes de compilar
+    },
+    publicDir: false // Deshabilitamos el directorio público predeterminado porque ya estamos usando 'public' como raíz
+});
